b0bc5c5880d4a189a148bd675214c015b02c1fcb
arminsafie <arminsafaie92@gmail.com>
2024-03-18 10:06:56 +0330

    bug fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,7 +20,7 @@ const showTime = () => {
   }
 
   hour = hour < 10 ? "0" + hour : hour;
-  min = min < 10 ? "0 " + min : min;
+  min = min < 10 ? "0" + min : min;
   sec = sec < 10 ? "0" + sec : sec;
 
   let currentTime = hour + ":" + min + ":" + sec + " " + am_pm;
